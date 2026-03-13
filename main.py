d54385769689bf959f9ca6771ba06929b1f63c26
Ioannis <itsenempis@gmail.com>
2026-03-13 17:48:33 +0200

    Development branch init

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+
+class Recipe:
+    ''' Η βασική κλάση συνταγών μας '''
+    def __init__ (self, name, ingredients, category, difficulty, total_time):
+        self.name = name
+        self.ingredients = ingredients
+        self.category = category
+        self.difficulty = difficulty
+        self.total_time = total_time
+        
+
+    def print_recipe():
+        pass
+pass
